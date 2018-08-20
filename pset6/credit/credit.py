c3f80cdeacef2848d4b67bb3a0e9655eef6596b0
webpropopuli <webpropopuli@gmail.com>
2018-08-20 23:59:58 +0000

    fixed Credit bug

--- pset6/credit/credit.py
+++ pset6/credit/credit.py
@@ -17,7 +17,7 @@ def main():
 		testNum = get_int()  # get user input unless I give test value
 
 # Get first 2digits to determine card type
-	cardType = str(testNum)[0:2]
+	cardType = int(str(testNum)[0:2])
 
 # Find first power of 10 larger than number
 	while testNum >= pow(10, x):
@@ -28,6 +28,7 @@ def main():
 		cksumTotal = 9999;  # force fail
 		x=0
 
+	##print("Before loop, x is=", x)
 # From L->R, take each digit and either
 #  add (evens) or
 #  multiply (odd)
@@ -36,11 +37,11 @@ def main():
 
     # Get next digit
 		digit = int(testNum / powX);     # get digit value
-		print("digit", digit)
+		##print("digit", digit)
 
     # add Checksums
     # follow Luhrs, adding evens and mult/flatten odds
-		if((x % 2) == 0):
+		if((x % 2) != 0):
 			cksumTotal += digit;
 		else:
 			#printf("oddnum: digit is %d - ck_in=%d...", digit, cksumTotal);
@@ -50,31 +51,31 @@ def main():
 
 			cksumTotal = cksumTotal + n
 
-		print("chksum=", cksumTotal)
+		##print("chksum=", cksumTotal)
 
 		testNum = testNum - (digit * powX)  # decrement num by subtracting digit * powX
 
-		print("testnum=", testNum)
+		##print("testnum=", testNum)
 		x-=1
 
-	print("c", cksumTotal, cardType);
+	##print("c", cksumTotal, cardType);
 #define CARD_INVALID 0
 #define CARD_AMEX 1
 #define CARD_MC 2
 #define CARD_VISA 3
 
 	if (cksumTotal % 10 != 0):
-		print("chksum failure")
+		##print("chksum failure")
 		cardType = 0        # failed cksum
 	else:
 		if (cardType == 34 or cardType == 37):
-			cardType = 1;
+			cardType = 1
 		elif (cardType >=40 and cardType <=49):
-			cardType = 3;
+			cardType = 3
 		elif (cardType >=50 and cardType <=55):
-			cardType = 2;
+			cardType = 2
 		else:
-			cardType = 0;
+			cardType = 0
 
 
 	print(cardTypes[cardType])
