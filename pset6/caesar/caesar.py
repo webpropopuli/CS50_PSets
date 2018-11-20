40bed0433a5484d088d152f0abe5bae0a5fda721
webpropopuli <webpropopuli@gmail.com>
2018-11-20 17:09:49 +0000

    PSet6 done (I forgot to do Caesar last time)

--- pset6/caesar/caesar.py
+++ pset6/caesar/caesar.py
@@ -6,34 +6,39 @@ from inspect import signature
 	# Only 1 arg allowed
 if (len(sys.argv) != 2):
     print("Wrong number of arguments. Exiting...");
-    sys.exit(0)
+    sys.exit(1)
 
-keyval = int(sys.argv[1]) % 26;
-print("keyval % 26=", keyval)
+iKeyval = int(sys.argv[1]) % 26;
+#print("keyval % 26=", iKeyval)
 
  # Get plaintext from user
 print("Plaintext: ", end="");
 strPlain = list(get_string())
+#print(strPlain)
 
 
 #ch  #current char in plaintest
 #A2aDiff  # how far to shift from 'A' to 'a'
-strCipher = []
+strCipher = ""
+
+#c=(pi+k)mod26
 
 # Shift each letter to cipher array
 #len = len(strPlain)
 for x in range(0, len(strPlain)):
-    print('x=', x)
     ch = strPlain[x];
+    #print("x={}->{}".format(x, ch))
+
 
     if(not ch.isalpha()):
-        strCipher[x] = ch;
+        strCipher += ch;
     else:
         if (ch.islower()):
-            A2aDiff = 'a'
+            A2aDiff = ord('a')
         else:
-            A2aDiff = 'A'
+            A2aDiff = ord('A')
 
-        strCipher[x] = chr( ((((ch - A2aDiff) + keyval) % 26)) + A2aDiff);
+        ec = chr(((ord(ch) + iKeyval - A2aDiff) %26) + A2aDiff);
+        strCipher += ec;
 
-print("ciphertext: ", strCipher);
+print("ciphertext: ",str(strCipher));
